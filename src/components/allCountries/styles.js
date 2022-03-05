3c91b83ef96514d300cc3c96ca037b7198039fa4
Pedro <pedrosodredev@gmail.com>
2022-03-05 20:49:17 -0300

    add Container and Header Style Component

---
+++ src/components/allCountries/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 80%;
+    margin: auto;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    padding: 30px;
+`;
+
