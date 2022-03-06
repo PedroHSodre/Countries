c63135b5e3ae0fefce8d204b1d29edd05a417a5a
Pedro <pedrosodredev@gmail.com>
2022-03-06 00:08:41 -0300

    all countries fully working

--- src/components/allCountries/index.js
+++ src/components/allCountries/index.js
@@ -1,24 +1,40 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import CountryView from "../countryView";
 import SearchBar from "../searchBar";
+import { Container, Header, Grid } from './styles';
 
 async function getCountries(){
+    let data;
     await axios.get('https://restcountries.com/v2/all')
-        .then(response => console.log(response))
+        .then(response => {data = response.data})
         .catch(err => console.log(err));
+
+    return data;
 }
 
 const AllContries = ({setCountryDetail}) => {
+    const [countries, setCountries] = useState([]);
+
     useEffect(() => {
-        getCountries();
+        getCountries().then(res => setCountries(res));
     }, []);
     return (
-        <>
-            <SearchBar />
-            <div>
-                
-            </div>
-        </>
+        <Container>
+            <Header>
+                <SearchBar />
+                <div>
+                    Filtro    
+                </div>
+            </Header>
+            <Grid>
+                {
+                    countries.map(country => (
+                        <CountryView country={country} />
+                    ))
+                }
+            </Grid>
+        </Container>
     )
 }
 
