0cea4b3296c6da5f04d3fbd718c12b6bd3bbbd86
Pedro <pedrosodredev@gmail.com>
2022-03-08 13:31:34 -0300

    add some changes to dark theme

--- src/styles/Themes/dark.js
+++ src/styles/Themes/dark.js
@@ -4,7 +4,7 @@ export default {
     colors: {
         elements: 'hsl(209, 23%, 22%)',
         background: 'hsl(207, 26%, 17%)',
-        text: 'hsl(0, 0%, 100%)'
+        text: 'hsl(0, 0%, 100%)',
     },
     fonts: {}
 }
