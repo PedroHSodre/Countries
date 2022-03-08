1db923f926d16fc6bad65b1b3b44069534868bde
Pedro <pedrosodredev@gmail.com>
2022-03-08 13:26:32 -0300

    created searchbar component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
-const SearchBar = () => {
+const Filter = styled.input`
+    background-color: ${props => props.theme.colors.elements};
+    color: ${props => props.theme.colors.text};
+    border: none;
+    outline: none;
+    padding: 15px 20px;
+    width: 300px;
+
+    &:focus{
+        outline: none;
+    }
+
+    @media(max-width: 600px){
+        width: 200px;
+    }
+
+    @media(max-width: 450px){
+        width: 125px;
+    }
+`
+
+const SearchBar = ({setFilter}) => {
     return (
         <>
-            <div>SearchBar</div>
-            <div>Filtro</div>
+            <Filter 
+                placeholder="Search country" 
+                onChange={(e) => setFilter(e.target.value)} 
+            />
         </>
     )
 }
